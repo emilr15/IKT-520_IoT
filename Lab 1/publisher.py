219ecbde310522f7f9f9756d7a26fdcc86616c67
emilr15 <emilmruud@gmail.com>
2020-02-17 10:35:52 +0100

    cleaned up pub and sub in lab 1. removed subscribe function from publisher (because it was unnecessary)

--- Lab 1/publisher.py
+++ Lab 1/publisher.py
@@ -4,34 +4,34 @@ from time import sleep
 client_name = 'my_publisher'
 broker_ip = '127.0.0.1'
 broker_port = 1883
+qos_level = 2
 topic = 'my_first_topic'
 msg = 'ping'
-msg2 = 'pingelipong'
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print(f'{client_name} is connected to broker on port: {broker_port}')
-        client.subscribe(topic, qos=0)
     else:
         print('Connection failed')
 
 def on_publish(client, userdata, mid):
     print(f'message with ID: |{mid}| was delivered to broker.')
 
-
-def on_subscribe(client):
-    pass
-
 def run_publisher():
+    # Initialising client
     client = mqtt.Client(client_name)
+
+    # Declaring callback-functions for connecting and publishing
     client.on_connect = on_connect
     client.on_publish = on_publish
-    client.connect(host=broker_ip, port=broker_port)
-    client.publish(topic, msg, qos=0)
-    for i in range(5):
-        sleep(0.5)
-        client.publish(topic, msg2, qos=1)
 
+    # Connecting to broker
+    client.connect(host=broker_ip, port=broker_port)
+
+    # Publishing message to broker
+    client.publish(topic, msg, qos=qos_level)
+
+    # Starting loop
     client.loop_forever()  # start the loop
 
 
--- Lab 1/subscriber.py
+++ Lab 1/subscriber.py
@@ -7,6 +7,7 @@ broker_ip = '127.0.0.1'
 broker_port = 1883
 topic = 'my_first_topic'
 msg = 'ping'
+qos_level = 2
 
 
 def on_connect(client, userdata, flags, rc):
@@ -17,13 +18,19 @@ def on_connect(client, userdata, flags, rc):
         print('Connection failed')
 
 def on_message(client, userdata, message):
+    # Checking if message payload is 'ping', if yes 'pong' is printed
     if message.payload.decode() == msg:
         print('pong')
 
 def run_subscriber():
+    # Initialising client
     client = mqtt.Client(client_name)
+
+    # Declaring callback-functions for connecting and receiving messages
     client.on_connect = on_connect
     client.on_message = on_message
+
+    # Connecting to broker
     client.connect(host=broker_ip, port=broker_port)
 
     client.loop_forever()
